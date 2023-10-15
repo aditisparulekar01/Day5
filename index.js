b58bc08ed2eab048fdc8a70aadd3f7ac5eafd076
aditisparulekar01 <147847864+aditisparulekar01@users.noreply.github.com>
2023-10-15 15:26:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+// //prompt("Enter your name:","");
+// //console.log("this is console");
+// //console.log(6+5);
+// //document.write("<h1>JAVASCRIPT</h1>");
+// var num1=5;
+// var num1=6;//we can redeclare var
+// num1=8//we can reassign
+// console.log(num1);
+// let num2=9;
+// //let num2=10;//we cant redeclare
+// num2=11;//we can reassign
+// console.log(num2);
+// const num3=10;
+// //const num3=11;we cannot redeclare
+// //num3=12;we cannot reassign the value
+// console.log(num3);
+// console.log(3**2);
+// let x=5
+// x++
+// console.log(x);
+// let num1=5
+// let num2="5"
+// console.log(num1==num2);
+// console.log(num1===num2);
+// function mul(n1,n2,n3,n4){
+//     return n1*n2*n3*n4;
+// }
+// document.write(mul(2,3,4,5));
+// function greet(){
+//     alert("hello");
+// // }
+// let age=20
+// if(age>=18){
+//     document.write("ëligible to vote");
+// }else{
+//     document.write("Not eligible to vote");
+// }
+// for(let n=10;n>=1;n--){
+//     console.log(n);
+// }
+// let n=5
+// while(n--){
+//     console.log(n);
+// }
+// let e=document.getElementById("head").innerHTML;
+// document.write(e);
+let el=document.querySelector("#head").innerHTML;
+console.log(el);
